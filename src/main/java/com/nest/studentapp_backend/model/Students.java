3b2b65f7c42c2ebf0a44b3e75ec7cc158a24f9dc
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-06 09:58:10 +0530

    add

---
+++ src/main/java/com/nest/studentapp_backend/model/Students.java
@@ -0,0 +1,70 @@
+package com.nest.studentapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Students {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String admno;
+    private int rollno;
+    private String college;
+
+    public Students() {
+    }
+
+    public Students(int id, String name, String admno, int rollno, String college) {
+        this.id = id;
+        this.name = name;
+        this.admno = admno;
+        this.rollno = rollno;
+        this.college = college;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdmno() {
+        return admno;
+    }
+
+    public void setAdmno(String admno) {
+        this.admno = admno;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+}
